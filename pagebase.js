a0a9ef5201728f22d0d28ec6ff0cb17cc1fadaaa
Richard Wang <richard@realdirect.com>
2014-03-15 18:33:58 -0400

    added initial code for storing urls in localStorage

--- pagebase.js
+++ pagebase.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
+	if (!localStorage.pageBase) localStorage.pageBase = JSON.stringify([]);
+	var cloned = $.parseJSON(localStorage.pageBase);
+	$.each(cloned, function(i, url) {
+		$('#urls').append('<a href="' + url + '">' + url + '</a>');
+	})
 	$('#foo').click(function() {
 		chrome.tabs.query({
 			active: true,
 			currentWindow: true
 		}, function(tabs) {
+			var cloned = $.parseJSON(localStorage.pageBase);
+			cloned.push(tabs[0].url);
+			localStorage.pageBase = JSON.stringify(cloned);
 			console.log(tabs[0].url);
 		});
 	})
