e3be77e49946192d6095a7706d52645ea66a2ce7
Richard Wang <richard@realdirect.com>
2014-03-16 22:03:45 -0400

    basic functionality for adding, deleting, and loading saved pages

--- pagebase.js
+++ pagebase.js
@@ -1,18 +1,38 @@
 $(document).ready(function() {
-	if (!localStorage.pageBase) localStorage.pageBase = JSON.stringify([]);
-	var cloned = $.parseJSON(localStorage.pageBase);
-	$.each(cloned, function(i, url) {
-		$('#urls').append('<a href="' + url + '">' + url + '</a>');
-	})
+	var init = function() {
+		$('#urls').empty();
+		if (!localStorage.pageBase) localStorage.pageBase = JSON.stringify([]);
+		var cloned = $.parseJSON(localStorage.pageBase);
+		$.each(cloned, function(i, page) {
+			$('#urls').append('<a href="' + page.url + '">' + page.title + '</a> <a href="#" class="delete-page" data-id="'+ i + '">x</a><br/>');
+		});
+
 	$('#foo').click(function() {
 		chrome.tabs.query({
 			active: true,
 			currentWindow: true
 		}, function(tabs) {
 			var cloned = $.parseJSON(localStorage.pageBase);
-			cloned.push(tabs[0].url);
+			cloned.push({
+				url: tabs[0].url,
+				title: tabs[0].title
+			});
 			localStorage.pageBase = JSON.stringify(cloned);
 			console.log(tabs[0].url);
+			init();
 		});
-	})
+	});
+	$('#urls .delete-page').click(function(event) {
+		event.preventDefault();
+		var page_id = $(event.target).data('id');
+		var cloned = $.parseJSON(localStorage.pageBase);
+		cloned.splice(page_id, 1);
+		localStorage.pageBase = JSON.stringify(cloned);
+		init();
+	});
+	};
+
+	init();
+
+
 });
